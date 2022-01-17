60eebd280ff455afb848633b3664c602744e2da2
Verma <ravi.verma@diageo.com>
2022-01-17 17:47:22 +0530

    list view table fragment design

--- zsaleinvoices/webapp/controller/ListView.controller.js
+++ zsaleinvoices/webapp/controller/ListView.controller.js
@@ -1,9 +1,13 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function(Controller) {
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/model/json/JSONModel"
+], function(Controller, JSONModel) {
 	"use strict";
 
 	return Controller.extend("com.diageo.csd.saleinvoiceszsaleinvoices.controller.ListView", {
-
+		onInit: function() {
+			this.getView().setModel(new JSONModel(), "this");
+			this.getView().getModel("this").setProperty("/selectedOrders", {});
+		}
 	});
 });
